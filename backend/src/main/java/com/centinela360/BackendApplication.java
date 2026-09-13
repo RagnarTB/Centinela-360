6cd0618b622a97f57d936328f3d6abb390cfb605
RagnarTB <ldtb2001@gmail.com>
2026-09-12 23:49:29 -0500

    feat(backend): configuracion R2DBC y Kafka en application.yml

--- backend/src/main/java/com/centinela360/BackendApplication.java
+++ backend/src/main/java/com/centinela360/BackendApplication.java
@@ -2,10 +2,8 @@ package com.centinela360;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.r2dbc.autoconfigure.R2dbcAutoConfiguration;
 
-@SpringBootApplication(exclude = { R2dbcAutoConfiguration.class }) // use exclude para que no me pida configuracion db
-																	// para probar el backend
+@SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
